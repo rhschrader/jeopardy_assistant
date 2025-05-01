cb257855d73b367da69c7560def7ca44a42fd50b
Ross Schrader <82004650+rhschrader@users.noreply.github.com>
2025-05-01 12:36:59 -0600

    I need to stop with stupid errors

--- scripts/load_data_original.py
+++ scripts/load_data_original.py
@@ -31,7 +31,6 @@ def insert_data():
 
     # Load data and cleanse data
     df = cleanse_data('../data/combined_season1-40.tsv', sep = '\t')
-    df = combine_columns(df) # Combine columns for embedding
 
     # SQL INSERT statement
     insert_sql = """
