636606147cadf69ad52493eb053352920f716770
Ross Schrader <82004650+rhschrader@users.noreply.github.com>
2025-05-27 17:10:03 -0600

    adding hnsw index to improve similarity search speed

--- scripts/create_hnsw_index.py
+++ scripts/create_hnsw_index.py
@@ -4,7 +4,7 @@ from db_functions import PostGreSQL
 
 if __name__ == "__main__":
     # Create a Postgres client
-    postgres_client = PostGreSQL()
+    postgres_client = PostGreSQL(create_psycopg2_connection = True)
 
     # Create the index
     postgres_client.create_hnsw_index(table_name='jeopardy_embeddings')
